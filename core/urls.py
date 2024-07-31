d6549363e1e171fe9a5189e9340b158b2fc4c530
cuarty <dibartolo.fx@gmail.com>
2024-07-31 18:49:27 -0300

    actualizacion_blog

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import HomeView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", HomeView.as_view(), name="home")
-    #Al poner "" sin nada en su interior, indica que la página principal presentará la vista HomeView 
-    #Utilizamos el .as_view() porque estamos trabajando con una clase, de no utilizarla el programa no funcionaría correctamente:
-    #Colocamos el name para hacer referencia a la clase
+    path("", HomeView.as_view(), name="home"),
+    path('blog', include('blog.urls', namespace='blog'))
+    
 ]
