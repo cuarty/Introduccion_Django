b1c701ae4d8a21ffb357c536eea4841e7a8a9e96
cuarty <dibartolo.fx@gmail.com>
2024-08-10 11:13:41 -0300

    nueva actualización

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import View
+from django.views.generic import View, UpdateView, DeleteView
 from .forms import PostCreateForm
 from .models import Post
+from django.urls import reverse_lazy
 
 
 class BlogListView(View):
@@ -12,6 +13,7 @@ class BlogListView(View):
         }
         return render(request, "blog_list.html", context)
     
+
 class BlogCreateView(View):
     def get(self, request, *args, **kwargs):
         form = PostCreateForm()
@@ -36,6 +38,7 @@ class BlogCreateView(View):
         }
         return render(request, 'blog_create.html', context)
 
+
 class BlogDetailView(View):
     def get(self, request, pk, *args, **kwargs):
         post= get_object_or_404(Post, pk=pk)
@@ -43,3 +46,19 @@ class BlogDetailView(View):
             'post':post
         }
         return render(request,"blog_detail.html" ,context)
+
+
+class BlogUpdateView(UpdateView):
+    model=Post
+    fields=['title','content']
+    template_name='blog_update.html'
+
+    def get_success_url(self):
+        pk= self.kwargs['pk']
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
+
+
+class BlogDeleteView(DeleteView):
+    model= Post
+    template_name= 'blog_delete.html'
+    success_url= reverse_lazy('blog:home')
